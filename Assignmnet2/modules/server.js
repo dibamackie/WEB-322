5873ff8e56239c265bcf1ef3023734b504ed897e
Diba Makki <sdmakki@myseneca.ca>
2024-06-06 21:56:58 -0400

    ready to deploy

---
+++ Assignmnet2/modules/server.js
@@ -0,0 +1,43 @@
+/********************************************************************************
+* WEB322 – Assignment 02
+*
+* I declare that this assignment is my own work in accordance with Seneca's
+* Academic Integrity Policy:
+*
+* https://www.senecacollege.ca/about/policies/academic-integrity-policy.html
+*
+* Name: _____DIBA_MAKKI____ Student ID: _____144420189____ Date: ___MAY_31,_2024___
+*
+* Published URL: ___________________________________________________________
+*
+********************************************************************************/
+//web frame for node.js
+const express = require('express');
+//manages logoset data
+const legoData = require('./modules/legoSets');
+
+const app = express();
+const port = 3000;
+
+// URL message 
+app.get('/', (req, res) => { // defines the root for URL
+    res.send('Welcome to the LEGO Sets Service!');
+});
+
+
+app.get('/lego/sets', async (req, res) => {
+    try {
+        await legoData.initialize();
+        const allSets = await legoData.getAllSets();
+        res.json(allSets);
+    } catch (error) {
+        console.error(error);  // it prints error
+        res.status(500).send("An error occurred: " + error.message);
+    }
+});
+
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+});
+
